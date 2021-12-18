4b6b0730829ade56c1ad1ccec558d851a9ba2a8c
joseduranz <josedrn@gmail.com>
2021-12-18 18:40:25 -0500

    2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/FrontendConcecionario'));
+app.use(express.static('/dist/FrontendConcecionario'));
 
 // Send all requests to index.html
 app.get('/*', (req, res) =>{
